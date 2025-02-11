e474e7db9b565f9035d165bf1fb7e09d0c7c512a
hyunsoopark <hyunsoopark4@naver.com>
2025-02-11 23:36:22 +0900

    feat : Noise cancling - Canny edge 임계값 조정 - Gaussian Blur 추가(노이즈 감소) - ORB 특징점 필터링(response > 0.02)

--- ORB_Test.py
+++ ORB_Test.py
@@ -12,18 +12,23 @@ gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # ORB는 흑백 이미지를 사용
 
 
 # 이미지 대비 향상(CLAHE 적용)
-#clahe = cv.createCLAHE(clipLimit = 3.5, tileGridSize = (8,8)) 
-#gray = clahe.apply(gray)
+# clahe = cv.createCLAHE(clipLimit = 3.5, tileGridSize = (8,8)) 
+# gray = clahe.apply(gray)
+
+
+# 노이즈 제거를 위한 GaussianBlur 적용 
+gray_blur = cv.GaussianBlur(gray,(5, 5),0)
+
 
 
 # 대비 향상을 위한 Adaptive Threshold 적용 
-adaptive_thresh = cv.adaptiveThreshold(
-    gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2
-)
+# adaptive_thresh = cv.adaptiveThreshold(
+#     gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2
+# )
 
 
 # Conny 엣지 검출적용 (경계선 강조) 
-edges = cv.Canny(adaptive_thresh, 50, 150)
+edges = cv.Canny(gray_blur, 100, 200)
 
 
 # ORB 객체 생성 
@@ -36,17 +41,17 @@ keypoints, descriptors = orb.detectAndCompute(edges, None)
 
 
 # 특징점 필터링(반응값 기준)
-#filtered_keypoints = [kp for kp in keypoints if kp.response > 0.01] # 너무 작은 특징 제거 
+filtered_keypoints = [kp for kp in keypoints if kp.response > 0.02] # 너무 작은 특징 제거 
 
 
 # 특징점 그리기 
-#output_image = cv.drawKeypoints(image, filtered_keypoints, None, (0, 255, 0), flags = 0)
-output_image = cv.drawKeypoints(image, keypoints, None, (0, 255, 0), flags = 0)
+output_image = cv.drawKeypoints(image, filtered_keypoints, None, (0, 255, 0), flags = 0)
+# output_image = cv.drawKeypoints(image, keypoints, None, (0, 255, 0), flags = 0)
 
 # 결과 출력 
 while True:
-    cv.imshow("ORB Keypoints", output_image)
-    cv.imshow("ORB Check", edges)
+    cv.imshow("ORB Keypoints (Filtered)", output_image)
+    cv.imshow("Canny Edge (Filtered)", edges)
 
     if cv.waitKey(1) & 0xFF == ord('q'):   # 프로그램 종료
         break
