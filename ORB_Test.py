9edbd27945b9211dfc6f132fff863d7a6dab98f4
hyunsoopark <hyunsoopark4@naver.com>
2025-02-12 22:07:30 +0900

    feat : 대리석 인식 개선 및 특징점 필터 수정 - ORB의 response값 수정 - nfeatures 값을 증가시켜 더 많은 특징점 감지

--- ORB_Test.py
+++ ORB_Test.py
@@ -2,13 +2,35 @@ import cv2 as cv
 import numpy as np
 
 # 이미지 불러오기 
-image = cv.imread("many_shoes.jpg") # 여기에 사용할 이미지 경로 입력
+image = cv.imread("close_shoes.jpg") # 여기에 사용할 이미지 경로 입력
 image = cv.resize(image,(600,800))
+
+
+# HSV 변환
+hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+
+# 회색 대리석 필터링(예상되는 회색 범위 설정)
+lower_gray = np.array([0, 0, 80])   #HSV에서 어두운 회색
+upper_gray = np.array([180, 50, 220])   # 밝은 회색
+
+
+# 회색 영역 마스크 생성
+gray_mask = cv.inRange(hsv, lower_gray, upper_gray)
+
+
+# 마스크 반전 (회색이 아닌 부분만 남기기)
+non_gray_mask = cv.bitwise_not(gray_mask)
+
+
+# 원본 이미지에서 회색 제거 
+filtered_image = cv.bitwise_and(image, image, mask=non_gray_mask)
 
 
 
 # 흑백변환 
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # ORB는 흑백 이미지를 사용함
+# gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # ORB는 흑백 이미지를 사용함(그레이스케일 변환)
+gray = cv.cvtColor(filtered_image, cv.COLOR_BGR2GRAY)   # 그레이스케일 변환
 
 
 # 이미지 대비 향상(CLAHE 적용)
@@ -32,7 +54,7 @@ edges = cv.Canny(gray_blur, 100, 200)
 
 
 # ORB 객체 생성 
-orb = cv.ORB_create(nfeatures = 1000, WTA_K = 4, edgeThreshold = 10, patchSize = 25) # 기본값은 500
+orb = cv.ORB_create(nfeatures = 1500, WTA_K = 4, edgeThreshold = 10, patchSize = 30) # 기본값은 500
 
 
 
@@ -41,11 +63,12 @@ keypoints, descriptors = orb.detectAndCompute(edges, None)
 
 
 # 특징점 필터링(반응값 기준)
-filtered_keypoints = [kp for kp in keypoints if kp.response > 0.02] # 너무 작은 특징 제거 
+filtered_keypoints = [kp for kp in keypoints if 0.05 < kp.response < 0.2]
 
 
-# 특징점 그리기 
-output_image = cv.drawKeypoints(image, filtered_keypoints, None, (0, 255, 0), flags = 0)
+# 특징점 그리기
+# output_image = cv.drawKeypoints(image, filtered_keypoints, None, (0, 255, 0), flags = 0)
+output_image = cv.drawKeypoints(filtered_image, filtered_keypoints, None, (0, 255, 0), flags = 0)
 # output_image = cv.drawKeypoints(image, keypoints, None, (0, 255, 0), flags = 0)
 
 # 결과 출력 
